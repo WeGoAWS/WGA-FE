1b95522ab39ce886cd23db89dc8de81446e0d1ab
newbiehwang <137700401+newbiehwang@users.noreply.github.com>
2025-04-02 23:36:25 +0900

    Fix: 환경변수 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     // 환경 변수 로드 (접두사 없이)
@@ -17,6 +18,15 @@ export default defineConfig(({ mode }) => {
         },
         server: {
             port: 5173, // 리다이렉트 URI와 일치하도록 변경
+            proxy: {
+                '/api': {  //#1. axios라이브러리 등으로 http 요청인데 api로 시작하면,
+                  target: env.API_DEST,  //#2. 이쪽 주소로 매핑하여 백그라운드로 보내라.
+                  changeOrigin: true,
+                  rewrite: (path) => path.replace(/^\/api/, ''),
+                  secure: false,
+                  ws: true
+                }
+            }
         },
         // 환경 변수를 클라이언트에 노출
         define: {
@@ -33,6 +43,7 @@ export default defineConfig(({ mode }) => {
             'import.meta.env.COGNITO_IDENTITY_POOL_ID': JSON.stringify(
                 env.COGNITO_IDENTITY_POOL_ID,
             ),
+            'import.meta.env.API_DEST': JSON.stringify(env.API_DEST)
         },
     };
 });
